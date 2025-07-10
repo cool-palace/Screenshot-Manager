0959be965cedc9e705fb9b8ae6b72a72fecf1188
Cool Palace <wynaut@yandex.ru>
2025-07-11 01:48:16 +0300

    QueryFilters переделан в синглтон, HashtagFilter содержит id

--- include/query_filters.h
+++ include/query_filters.h
@@ -3,27 +3,42 @@
 
 #include <QDateTime>
 #include <QList>
+#include <QMap>
 #include <set>
 
 class HashtagFilter {
+public:
     enum Code {
         BOTH    = -1,
         AMPTAG  = 0,
         HASHTAG = 1,
     };
-public:
-    HashtagFilter(const QString& tag, QChar c): m_tag(tag) {
-        if (c == " ") m_code = BOTH;
-        if (c == "&") m_code = AMPTAG;
-        if (c == "#") m_code = HASHTAG;
+    HashtagFilter() = default;
+    HashtagFilter(int id, const QChar& c = ' '): m_id(id) {
+        if (c == " ")      m_code = BOTH;
+        else if (c == "&") m_code = AMPTAG;
+        else if (c == "#") m_code = HASHTAG;
+    }
+    int id() const { return m_id; }
+    Code code() const { return m_code; }
+    QString sign() const {
+        if (m_code == BOTH)         return "";
+        else if (m_code == AMPTAG)  return "&";
+        else if (m_code == HASHTAG) return "#";
+        return "";
     }
     QString to_sql() const {
         if (m_code == HashtagFilter::BOTH)
-            return QString("('%1', NULL)").arg(m_tag);
-        return QString("('%1', %2)").arg(m_tag).arg(static_cast<int>(m_code));
+            return QString("(%1, NULL)").arg(m_id);
+        return QString("(%1, %2)").arg(m_id).arg(static_cast<int>(m_code));
+    }
+    bool operator<(const HashtagFilter& other) const {
+        if (m_id != other.m_id)
+            return m_id < other.m_id;
+        return m_code < other.m_code;
     }
 private:
-    QString m_tag;
+    int m_id;
     Code m_code = BOTH;
 };
 
@@ -56,20 +71,29 @@ struct TextFilter {
 };
 
 struct HashtagsFilter {
-    bool enabled = false;
-    QList<HashtagFilter> included;
-    QList<HashtagFilter> excluded;
+    bool enabled = true;
+    QMap<int, HashtagFilter> included;
+    QMap<int, HashtagFilter> excluded;
 };
 
 struct QueryFilters {
+private:
+    QueryFilters() {}
+public:
+    static QueryFilters& instance() {
+        static QueryFilters instance;
+        return instance;
+    }
+    QueryFilters(const QueryFilters&) = delete;
+    QueryFilters& operator=(const QueryFilters&) = delete;
+    ~QueryFilters() {}
+public:
     PublicityFilter publicity;
     QuantityFilter quantity;
     LastUsedFilter last_used;
     SeriesFilter series;
     HashtagsFilter hashtags;
     TextFilter text;
-    uint32_t size = 1;
-    bool ordered = false;
 };
 
 #endif // QUERYFILTERS_H
