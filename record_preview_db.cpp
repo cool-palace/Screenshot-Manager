2057cf242b0912611bd513ee8054ca134124d15e
Cool Palace <wynaut@yandex.ru>
2025-07-23 01:50:05 +0300

    Изменения в RecordPreviewDB

--- record_preview_db.cpp
+++ record_preview_db.cpp
@@ -1,16 +1,6 @@
 #include "record_preview_db.h"
 #include "records_dialog.h"
-#include <include/database.h>
 #include <QSqlQuery>
-
-RecordPreviewInfo::RecordPreviewInfo(const QSqlQuery &query) {
-    id = query.value("record_id").toInt();
-    date = query.value("date").toDateTime();
-    quote = query.value("quote").toString();
-    hashtags = query.value("tag_string").toString();
-    title = query.value("title_name").toString();
-    links = query.value("links").toString().split('|');
-}
 
 RecordPreviewDB::RecordPreviewDB(const RecordPreviewInfo& record, const QDateTime& time, QWidget *parent) :
     QWidget(parent), m_info(std::move(record)), m_time(time)
@@ -18,22 +8,32 @@ RecordPreviewDB::RecordPreviewDB(const RecordPreviewInfo& record, const QDateTim
     setupUi(this);
     pbSelect->hide();
     update();
+    update_time();
     connect(pbReroll, &QPushButton::clicked, this, &RecordPreviewDB::reroll);
     connect(pbTime, &QPushButton::clicked, this, &RecordPreviewDB::set_time);
     connect(pbSearch, &QPushButton::clicked, this, &RecordPreviewDB::search);
     connect(pbNumber, &QPushButton::clicked, this, &RecordPreviewDB::input_number);
     connect(pbSwitchUp, &QPushButton::clicked, this, &RecordPreviewDB::switch_up);
     connect(pbSwitchDown, &QPushButton::clicked, this, &RecordPreviewDB::switch_down);
-    pbTime->setToolTip(QString("Время публикации: %1").arg(time.time().toString("hh:mm")));
+}
+
+RecordPreviewDB::~RecordPreviewDB() {
+    if (m_next)
+        m_next->m_prev = m_prev;
+    if (m_prev)
+        m_prev->m_next = m_next;
 }
 
 void RecordPreviewDB::set_index(int id) {
     QSqlQuery query;
     Database::instance().select_record_by_id(query, id);
-    read_record_info(query);
+    if (query.next()) {
+        m_info = RecordPreviewInfo(query);
+        update();
+    }
 }
 
-void RecordPreviewDB::set_record(const RecordPreviewInfo& record) {
+void RecordPreviewDB::set_record(const RecordPreviewInfo &record) {
     m_info = record;
     update();
 }
@@ -53,28 +53,6 @@ void RecordPreviewDB::update_log_info() {
     lblLogs->setFont(font);
 }
 
-void RecordPreviewDB::update() {
-    lblNumber->setText(QString().setNum(m_info.id));
-    update_text();
-    update_images();
-    update_log_info();
-}
-
-void RecordPreviewDB::reroll() {
-    QSqlQuery query;
-    Database::instance().select_records(query, QueryFilters::instance(), true, 1);
-    read_record_info(query);
-}
-
-void RecordPreviewDB::input_number() {
-    int max = Database::instance().count_records();
-    bool ok;
-    int index = QInputDialog::getInt(this, tr("Номер записи"),
-                                 tr("Введите номер записи от 1 до %1").arg(max), m_info.id, 1, max, 1, &ok);
-    if (!ok) return;
-    set_index(index);
-}
-
 void RecordPreviewDB::switch_up() {
     if (!m_prev) return;
     std::swap(m_info, m_prev->m_info);
@@ -82,7 +60,7 @@ void RecordPreviewDB::switch_up() {
     m_prev->update();
 }
 
-void RecordPreviewDB::switch_down() {
+void RecordPreviewDB::switch_down(){
     if (!m_next) return;
     std::swap(m_info, m_next->m_info);
     update();
@@ -100,8 +78,37 @@ void RecordPreviewDB::set_time() {
     TimeInputDialog dialog(m_time.time(), this);
     if (dialog.exec() == QDialog::Accepted) {
         m_time.setTime(dialog.selectedTime());
-        pbTime->setToolTip(QString("Время публикации: %1").arg(m_time.time().toString("hh:mm")));
+        update_time();
     }
+}
+
+void RecordPreviewDB::reroll() {
+    QSqlQuery query;
+    Database::instance().select_records(query, QueryFilters::instance(), true, 1);
+    if (query.next()) {
+        m_info = RecordPreviewInfo(query);
+        update();
+    }
+}
+
+void RecordPreviewDB::input_number() {
+    int max = Database::instance().count_records();
+    bool ok;
+    int index = QInputDialog::getInt(this, tr("Номер записи"),
+                                 tr("Введите номер записи от 1 до %1").arg(max), m_info.id, 1, max, 1, &ok);
+    if (!ok) return;
+    set_index(index);
+}
+
+void RecordPreviewDB::update() {
+    lblNumber->setText(QString().setNum(m_info.id));
+    update_text();
+    update_images();
+    update_log_info();
+}
+
+void RecordPreviewDB::update_time() {
+    pbTime->setToolTip(QString("Время публикации: %1").arg(m_time.time().toString("hh:mm")));
 }
 
 void RecordPreviewDB::update_images() {
@@ -123,11 +130,5 @@ void RecordPreviewDB::update_images() {
 
 void RecordPreviewDB::update_text() {
     lblText->setText(m_info.quote + '\n' + m_info.hashtags);
-}
-
-void RecordPreviewDB::read_record_info(QSqlQuery & query) {
-    if (query.next()) {
-        m_info = RecordPreviewInfo(query);
-    }
-    update();
+    lblTitle->setText("\t" + m_info.title);
 }
--- record_preview_db.h
+++ record_preview_db.h
@@ -5,18 +5,8 @@
 #include <QWidget>
 #include <include/recordpreview.h>
 #include <include/query_filters.h>
+#include "record_preview_info.h"
 #include <ui_record_preview_db.h>
-
-struct RecordPreviewInfo {
-    RecordPreviewInfo() = default;
-    RecordPreviewInfo(const QSqlQuery& query);
-    int id;
-    QDateTime date;
-    QString quote;
-    QString hashtags;
-    QString title;
-    QStringList links;
-};
 
 class RecordPreviewDB : public QWidget, public Ui_RecordPreviewDB
 {
@@ -24,7 +14,7 @@ class RecordPreviewDB : public QWidget, public Ui_RecordPreviewDB
 
 public:
     explicit RecordPreviewDB(const RecordPreviewInfo&, const QDateTime&, QWidget *parent = nullptr);
-    ~RecordPreviewDB() {}
+    ~RecordPreviewDB();
 
 public slots:
     void set_index(int);
@@ -34,20 +24,16 @@ public slots:
     void set_prev(RecordPreviewDB* prev) { m_prev = prev; }
 
 private slots:
-    void update();
-    void reroll();
-    void input_number();
     void switch_up();
     void switch_down();
     void search();
     void set_time();
+    void reroll();
+    void input_number();
+    void update();
+    void update_time();
     void update_images();
     void update_text();
-    void read_record_info(QSqlQuery&);
-
-signals:
-    void search_start(int);
-    void reroll_request(RecordPreviewDB*);
 
 private:
     RecordPreviewInfo m_info;
